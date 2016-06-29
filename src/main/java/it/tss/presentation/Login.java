2b1fbc13d5ec0e293cc3d853af5a3a2f7d33002c
tss <tss@debian-tss24>
2016-06-29 14:59:34 +0200

    commit con login funzionante!!

--- src/main/java/it/tss/presentation/Login.java
+++ src/main/java/it/tss/presentation/Login.java
@@ -12,9 +12,9 @@ import it.tss.business.entity.Utente;
 import it.tss.presentation.util.JsfUtil;
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
-
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+
 import javax.inject.Inject;
 
 import javax.inject.Named;
@@ -27,7 +27,8 @@ import javax.inject.Named;
 @SessionScoped
 public class Login implements Serializable {
 
-    private Utente user;
+    private String user;
+    private String pwd;
 
     @Inject
     Security security;
@@ -41,29 +42,42 @@ public class Login implements Serializable {
     @Inject
     UtenteSrv utentesrv;
 
-    public Utente getUser() {
+    public String getUser() {
         return user;
     }
 
-    public void setUser(Utente user) {
+    public void setUser(String user) {
         this.user = user;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
     }
 
     public String onLogin() {
 
-        boolean login = security.login(user.getUsername(), user.getPwd());
+        boolean login = security.login(user, pwd);
 
-        if(login){
-        
-        sessiondata.setLoggedUser(user.toString());
-            
-            
+        if (login) {
+
+            sessiondata.setLoggedUser(user);
+
+            JsfUtil.addSuccessMessage("Login effettuata!!");
             return "utente/List.jsf/faces-redirect=true";
 
+        } else {
+            FacesContext.getCurrentInstance().
+                    addMessage("", new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "Login non effettuato.",
+                            ""));
         }
-        
-        JsfUtil.addErrorMessage("LOGIN ERRATA!!");
-        return null;
+
+        return "";
+
     }
-    
+
 }
